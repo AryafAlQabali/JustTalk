0a0829def7072cb918b91e183709bbebbda3146b
Aryaf AlQabali <Aryaf.Aqeel.has.s@tuwaiq.edu.sa>
2021-12-29 16:50:09 +0300

    Done configure message

--- Just-Talk/Just-Talk/Controllers/MessageVC.swift
+++ Just-Talk/Just-Talk/Controllers/MessageVC.swift
@@ -18,6 +18,8 @@ class MessageVC: MessagesViewController {
     private var chatId = ""
     private var recipientId = ""
     private var recipientName = ""
+    let refereshController = UIRefreshControl()
+    let micBut = InputBarButtonItem()
     
     //MARK:- init
     init(chatId: String, recipientId: String, recipientName: String) {
@@ -40,10 +42,77 @@ class MessageVC: MessagesViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+configureMSGCollectionView()
+        configureMessageInputBar()
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
 
+    private func configureMSGCollectionView() {
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messageCellDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        
+        
+        
+        scrollsToLastItemOnKeyboardBeginsEditing = true
+        maintainPositionOnKeyboardFrameChanged = true
+        messagesCollectionView.refreshControl = refereshController
+        
+    }
+    
+    private func configureMessageInputBar() {
+        messageInputBar.delegate = self
+        
+        let attachBut = InputBarButtonItem()
+        attachBut.image = UIImage(systemName: "plus.circle",withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        
+        attachBut.setSize(CGSize(width: 30, height: 30), animated: false)
+        attachBut.onTouchUpInside { item in
+            print("Attaching")
 
+            //TODO:- Attach action
+        }
+        
+//        let micBut = InputBarButtonItem()
+        micBut.image = UIImage(systemName: "mic.circle.fill", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        micBut.setSize(CGSize(width: 30, height: 30), animated: false)
+        
+        
+        //Add gesture recognizer
+        messageInputBar.setStackViewItems([attachBut], forStack: .left, animated: false)
+        
+        messageInputBar.setStackViewItems([attachBut], forStack: .left, animated: false)
+        
+        //TODO:- Update mic But status
+        updateMicButStatus(show: true)
+        
+        
+        
+        
+        //تمنع المستخدم ليقوم ب بيست لل امج داخل المسج لل فيو
+        messageInputBar.inputTextView.isImagePasteEnabled = false
+        messageInputBar.backgroundView.backgroundColor = .systemBackground
+        messageInputBar.inputTextView.backgroundColor = .systemBackground
+    
+        
+        
+        
+    }
+    func updateMicButStatus (show: Bool) {
+        if show {
+            messageInputBar.setStackViewItems([micBut], forStack: .right, animated: false)
+            messageInputBar.setRightStackViewWidthConstant(to: 30, animated: false)
+        }else {
+            messageInputBar.setStackViewItems([messageInputBar.sendButton], forStack: .right, animated: false)
+            
+            messageInputBar.setRightStackViewWidthConstant(to: 55, animated: false)
+        }
+    }
 }
---
+++ Just-Talk/Just-Talk/Model/LocalMSG.swift
@@ -0,0 +1,36 @@
+//
+//  LocalMSG.swift
+//  Just-Talk
+//
+//  Created by AryafAlaqabali on 25/05/1443 AH.
+//
+
+import Foundation
+import RealmSwift
+
+class LocalMSG : Object, Codable{
+
+   @objc dynamic var id = ""
+    @objc dynamic var chatRoomId = ""
+    @objc dynamic var date = Data()
+    @objc dynamic var sederName = ""
+    @objc dynamic  var senderId = ""
+    @objc dynamic var senderinitials = ""
+    @objc dynamic  var readDate = Date()
+    @objc dynamic  var type = ""
+    @objc dynamic  var status = ""
+    @objc dynamic  var message = ""
+    @objc dynamic  var audioUrl = ""
+    @objc dynamic var videoUrl = ""
+    @objc dynamic var picturUrl = ""
+    @objc dynamic var latitude = 0.0
+    @objc dynamic var longitude = 0.0
+    @objc dynamic var audioDuration = 0.0
+
+    
+    override class func primaryKey() -> String? {
+        return "id"
+    }
+
+
+}
